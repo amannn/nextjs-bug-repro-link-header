48ad20d2c4b18d59595aed62da3bea55990c2f66
Jan Amann <jan@amann.work>
2024-10-16 12:43:51 +0200

    Break alternate link via next/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      {/* ❌ Removes the `link` header from the middleware */}
+      <Image
+        src="/image.png"
+        priority
+        alt="An image"
+        width={500}
+        height={500}
+      />
     </main>
   );
 }
