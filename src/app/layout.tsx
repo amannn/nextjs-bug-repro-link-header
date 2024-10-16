adcce778e583c8f893fb2356972a63481a340910
Jan Amann <jan@amann.work>
2024-10-16 12:41:01 +0200

    Break alternate link via next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {Inter} from 'next/font/google';
+
+// ❌ Removes the `link` header from the middleware
+// const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: "Create Next App",
