0af37255918b43bf43d31ce14be252986d45591a
Jan Amann <jan@amann.work>
2024-10-16 12:34:04 +0200

    add middleware (still working)

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import {NextRequest, NextResponse} from 'next/server';
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.next({
+      headers: {
+        link: '<https://example.com>; rel="alternate"; hreflang="x-default'
+      }
+    });
+  }
+}
